bc29a905cd3924054d74dd62467e54356d68c2d3
Arik92 <arik92@gmail.com>
2017-02-21 17:42:47 +0200

    did some extensions

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
 var fetch = function () {
   $.ajax({
     method: "GET",
-    url: 'https://www.googleapis.com/books/v1/volumes?q='+$('#input').val(),
+    url: makeUrl(),
     dataType: "json",
     success: function(data) {
-      debugger;
-      var title = data.items[0].volumeInfo.title;
-      var desc = data.items[0].volumeInfo.description;
-      var auth = data.items[0].volumeInfo.authors[0];
-      var img= data.items[0].volumeInfo.imageLinks.thumbnail;
+      $('.print').empty();
+      for (var i = 0;i < 9;i++){
+      var title = data.items[i].volumeInfo.title;
+      var desc = data.items[i].volumeInfo.description;
+      var auth = data.items[i].volumeInfo.authors[0];
+      var img= data.items[i].volumeInfo.imageLinks.thumbnail;
       display(title, desc, auth, img);
       console.log(data);
+    }//for
     },//success
     error: function(jqXHR, textStatus, errorThrown) {
       console.log(textStatus);
@@ -29,3 +31,36 @@ var display = function(title, desc, auth, img){
   var newHtml = template(res);
   $('.print').append(newHtml);
 }//dispaly
+$(document).on('click','h1',function(){
+  debugger;
+  var title= $(this).text();
+  $.ajax({
+    method: "GET",
+    url: 'https://www.googleapis.com/books/v1/volumes?q=intitle:'+title,
+    dataType: "json",
+    success: function(data) {
+      $('.print').empty();
+      var title = data.items[0].volumeInfo.title;
+      var desc = data.items[0].volumeInfo.description;
+      var auth = data.items[0].volumeInfo.authors[0];
+      var img= data.items[0].volumeInfo.imageLinks.thumbnail;
+      display(title, desc, auth, img);
+      console.log(data);
+    },//success
+    error: function(jqXHR, textStatus, errorThrown) {
+      console.log(textStatus);
+    }
+  });//ajax
+})//fetch2
+var makeUrl = function(){
+  var input=$("#isbn").val();
+  if (isNaN(input)){
+  var url= 'https://www.googleapis.com/books/v1/volumes?q=intitle:';
+  }// title search
+  else {
+    var url= 'https://www.googleapis.com/books/v1/volumes?q=isbn:';
+  }//isbn search
+  url+=input;
+  $('#isbn').val("");
+  return url;
+}
